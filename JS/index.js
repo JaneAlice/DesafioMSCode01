2e05d4236bff2bbbb5a8a4a8bec8d1015c7185d1
JaneAlice <jane2014cruz@gmail.com>
2021-09-25 21:27:55 -0300

    Ajustes no menu responsivo

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+//Abrindo e fechando menu no responsivo
+
+var menu_aberto_aberto = 0;
+
+$('#botao_abrir_menu_mobile').on('click', function(e){
+    
+	if(menu_aberto_aberto == 0){
+		abrirMenu();
+	}else{
+		fecharMenu();
+	}
+	
+});
+
+$('#icone_fechar_menu_mobile').on('click', function(e){
+    
+	if(menu_aberto_aberto == 0){
+		abrirMenu();
+	}else{
+		fecharMenu();
+	}
+    
+});
+
+function abrirMenu(){
+	$('#menu_mobile').show(500);
+	$('#botao_abrir_menu_mobile').addClass('botao-menu-mobile-ativo');
+	menu_aberto_aberto = 1;
+}
+
+function fecharMenu(){
+	$('#menu_mobile').hide(500);
+	$('#icone_fechar_menu_mobile').removeClass('botao-menu-mobile-ativo');
+	menu_aberto_aberto = 0;
+}
