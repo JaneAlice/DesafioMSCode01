e14d067396fe5d125671f1a65ad7a011d6af776c
JaneAlice <jane2014cruz@gmail.com>
2021-10-06 22:44:29 -0300

    Finalizaçao da section 'FAQ'

--- JS/index.js
+++ JS/index.js
@@ -6,6 +6,16 @@ $('#btn_abrir_menu_mobile').on('click', function(e){
     
 	if(menu_aberto_aberto == 0){
 		abrirMenu();
+		
+		var target = document.querySelector('.menu-mobile-animado');
+		var player = target.animate([
+			{transform: 'translate(0,0)'},
+			{transform: 'translate(260px,0)'},
+		],400)
+		player.addEventListener('finish', function () {
+			target.style.transform = 'translate(260px,0)'
+		});
+
 	}else{
 		fecharMenu();
 	}
@@ -23,37 +33,100 @@ $('#btn_fechar_menu_mobile').on('click', function(e){
 });
 
 function abrirMenu(){
-	$('#menu_mobile').show(500);
-	$('#btn_abrir_menu_mobile').addClass('botao-menu-mobile-ativo');
+	$('#menu_mobile').show(200);
 	menu_aberto_aberto = 1;
 }
 
 function fecharMenu(){
-	$('#menu_mobile').hide(500);
-	$('#btn_fechar_menu_mobile').removeClass('botao-menu-mobile-ativo');
+	$('#menu_mobile').hide(600);
 	menu_aberto_aberto = 0;
 }
 
+// Scroll do menu
+
+$(function() {
+	$(window).on("scroll", function() {
+		if($(window).scrollTop() > 100) {
+			$(".menu-fixed").addClass("menu-fixed-animado");
+		}else{
+			$(".menu-fixed").removeClass("menu-fixed-animado");
+		}
+	})
+});
+
+// Contador de números
+
+$(".counter").each(function() {
+	var $this = $(this),
+		  countTo = $this.attr("data-count");
+
+		$({ countNum: $this.text()}).animate({
+		countNum: countTo
+	},
+	{
+	duration: 3000,
+	easing:"linear",
+	step: function() {
+		$this.text(Math.floor(this.countNum));
+	},
+	complete: function() {
+		$this.text(this.countNum);
+	}
+  });
+});
+
 //
 
-$(document).ready(function(){
+$('#faq_um').on('click', function(){
+	$('#bloco_faq_um').show();
+	$('#bloco_faq_dois').hide();
+	$('#bloco_faq_tres').hide();
+	$('#bloco_faq_quatro').hide();
+	$('#bloco_faq_cinco').hide();
+	$('#bloco_faq_seis').hide();
+});
 
-	$(".counter").each(function() {
-	    var $this = $(this),
-	  	    countTo = $this.attr("data-count");
-  
-	        $({ countNum: $this.text()}).animate({
-	    	countNum: countTo
-	    },
-        {
-		duration: 3000,
-		easing:"linear",
-		step: function() {
-		    $this.text(Math.floor(this.countNum));
-		},
-		complete: function() {
-		    $this.text(this.countNum);
-		}
-	  });
-	});
+$('#faq_dois').on('click', function(){
+	$('#bloco_faq_um').hide();
+	$('#bloco_faq_dois').show();
+	$('#bloco_faq_tres').hide();
+	$('#bloco_faq_quatro').hide();
+	$('#bloco_faq_cinco').hide();
+	$('#bloco_faq_seis').hide();
+});
+
+$('#faq_tres').on('click', function(){
+	$('#bloco_faq_um').hide();
+	$('#bloco_faq_dois').hide();
+	$('#bloco_faq_tres').show();
+	$('#bloco_faq_quatro').hide();
+	$('#bloco_faq_cinco').hide();
+	$('#bloco_faq_seis').hide();
+});
+
+$('#faq_quatro').on('click', function(){
+	$('#bloco_faq_um').hide();
+	$('#bloco_faq_dois').hide();
+	$('#bloco_faq_tres').hide();
+	$('#bloco_faq_quatro').show();
+	$('#bloco_faq_cinco').hide();
+	$('#bloco_faq_seis').hide();
+});
+
+$('#faq_cinco').on('click', function(){
+	$('#bloco_faq_um').hide();
+	$('#bloco_faq_dois').hide();
+	$('#bloco_faq_tres').hide();
+	$('#bloco_faq_quatro').hide();
+	$('#bloco_faq_cinco').show();
+	$('#bloco_faq_seis').hide();
+});
+
+$('#faq_seis').on('click', function(){
+	$('#bloco_faq_um').hide();
+	$('#bloco_faq_dois').hide();
+	$('#bloco_faq_tres').hide();
+	$('#bloco_faq_quatro').hide();
+	$('#bloco_faq_cinco').hide();
+	$('#bloco_faq_seis').show();
 });
